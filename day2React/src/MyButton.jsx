3d221bc9e46236333df6fd098bb78a12027b5eb4
Shishir <mac@shihsir.local>
2024-09-22 14:06:11 +0545

    just basic one

--- day2React/src/MyButton.jsx
+++ day2React/src/MyButton.jsx
@@ -2,7 +2,7 @@
 const MyButton = (props) => {
   return (
     <>
-      <button className={props.owncss ? props.owncss:"bg-red-500 text-white px-4 py-2"}>Clicke me</button>
+      <button className={props.owncss ? `${props.owncss}`:"bg-red-500 text-white px-4 py-2"}>Clicke me</button>
     </>
   )
 }
